0583afef8862f0e226d3a9ccaa91bf50c86ae1b5
vLannaAi <138058952+vLannaAi@users.noreply.github.com>
2026-06-27 12:39:02 +0700

    fix(showcase): provide noydb-ui config from app.vue setup (was invalid in plugin)

--- examples/showcase/app/app.vue
+++ examples/showcase/app/app.vue
@@ -1,1 +1,8 @@
+<script setup lang="ts">
+import { provideNoydbUi } from '@noy-db/ui-nuxt/core'
+
+// TODO(Task 7): wire t + locale from useShowcaseI18n
+provideNoydbUi({ theme: 'system', locale: 'en' })
+</script>
+
 <template><NuxtLayout><NuxtPage /></NuxtLayout></template>
--- examples/showcase/app/plugins/vault.client.ts
+++
@@ -1,6 +0,0 @@
-import { provideNoydbUi } from '@noy-db/ui-nuxt/core'
-
-export default defineNuxtPlugin(() => {
-  // TODO(Task 7): wire t + locale from useShowcaseI18n
-  provideNoydbUi({ theme: 'system', locale: 'en' })
-})
